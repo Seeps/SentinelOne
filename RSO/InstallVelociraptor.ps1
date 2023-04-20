98a03476c0e2614ac6d10d9f4fedd9285b2299a3
Deepak Sharma <supersonic08@gmail.com>
2023-04-20 18:28:49 -0400

    Create InstallVelociraptor.ps1

---
+++ RSO/InstallVelociraptor.ps1
@@ -0,0 +1,21 @@
+# Utilize SentinelOne Remote Script Orchestration (RSO) & Automation to deploy Velociraptor
+
+function velociraptor(
+    [Parameter(Mandatory = $true)][string]$url
+) {    
+    $TempFolder = ([io.path]::GetTempPath())
+    $VelociraptorMSIPath = Join-Path $TempFolder "Win-CIR_Velociraptor.msi"
+
+    try {
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri "$url" -OutFile $VelociraptorMSIPath
+    }
+    catch {
+        Write-Error "Error downloading the Velociraptor package. Error $PSItem"
+        exit 1
+    }
+
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$VelociraptorMSIPath`" /qn" -Wait -NoNewWindow
+}
+
+velociraptor @Args
